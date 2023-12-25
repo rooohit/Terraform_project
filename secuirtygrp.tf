6dd33308efcd7db61e8393492394628f18e129f6
Rohit <roohitsingh17@gmail.com>
2023-12-25 19:08:38 +0530

    edited some changes to reslove issue

--- secuirtygrp.tf
+++ secuirtygrp.tf
@@ -44,10 +44,10 @@ resource "aws_security_group" "beanstalk_instance_sg" {
   description = "Security Group for Instances under Beanstlak"
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -71,8 +71,8 @@ resource "aws_security_group" "backend_services_sg" {
   }
 
   ingress {
-    from_port       = 22
-    to_port         = 22
+    from_port       = 0
+    to_port         = 0
     protocol        = "-1"
     security_groups = [aws_security_group.beanstalk_instance_sg.id]
   }
